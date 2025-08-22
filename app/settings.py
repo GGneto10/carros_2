54d302062f26a0e10b338b9f07b1ad981fe26d4d
Davi-D18 <luizdavi.code@gmail.com>
2025-08-22 16:04:14 -0300

    refactor(estrutura): corrigido alguns erros na estrutura e organizado alguns arquivos

--- app/settings.py
+++ app/settings.py
@@ -51,12 +51,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'app.app.urls'
+ROOT_URLCONF = 'app.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['app/app/templates'], 
+        'DIRS': [BASE_DIR / 'app/templates'], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'app.app.wsgi.application'
+WSGI_APPLICATION = 'app.wsgi.application'
 
 
 # Database
@@ -116,7 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'app/static'), # Local onde estão estáticos como JS e CSS
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
---
+++ app/static/js/base.js
@@ -0,0 +1,18 @@
+// Verifica o tema salvo no localStorage
+const currentTheme = localStorage.getItem('theme') || 'light';
+    
+    // Aplica o tema salvo
+    if (currentTheme === 'dark') {
+      document.body.classList.add('dark-mode');
+      document.getElementById('themeToggle').textContent = '☀️';
+    }
+
+    // Alternar tema
+    document.getElementById('themeToggle').addEventListener('click', function() {
+      document.body.classList.toggle('dark-mode');
+      
+      const isDark = document.body.classList.contains('dark-mode');
+      localStorage.setItem('theme', isDark ? 'dark' : 'light');
+      
+      this.textContent = isDark ? '☀️' : '🌙';
+    });
