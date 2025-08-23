5da32f9781cba615c9ad8321ad209c077ccad6ab
Davi-D18 <luizdavi.code@gmail.com>
2025-08-23 14:58:46 -0300

    feat(header): adicionado alerta informando que está em desenvolvimento

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -1,18 +1,22 @@
 // Verifica o tema salvo no localStorage
 const currentTheme = localStorage.getItem('theme') || 'light';
     
-    // Aplica o tema salvo
-    if (currentTheme === 'dark') {
-      document.body.classList.add('dark-mode');
-      document.getElementById('themeToggle').textContent = '☀️';
-    }
+// Aplica o tema salvo
+if (currentTheme === 'dark') {
+  document.body.classList.add('dark-mode');
+  document.getElementById('themeToggle').textContent = '☀️';
+}
 
-    // Alternar tema
-    document.getElementById('themeToggle').addEventListener('click', function() {
-      document.body.classList.toggle('dark-mode');
-      
-      const isDark = document.body.classList.contains('dark-mode');
-      localStorage.setItem('theme', isDark ? 'dark' : 'light');
-      
-      this.textContent = isDark ? '☀️' : '🌙';
-    });
+// Alternar tema
+document.getElementById('themeToggle').addEventListener('click', function() {
+  document.body.classList.toggle('dark-mode');
+  
+  const isDark = document.body.classList.contains('dark-mode');
+  localStorage.setItem('theme', isDark ? 'dark' : 'light');
+  
+  this.textContent = isDark ? '☀️' : '🌙';
+});
+
+document.querySelector('#link-contato').addEventListener('click', function() {
+  alert("Essa função ainda está em desenvolvimento")
+})
